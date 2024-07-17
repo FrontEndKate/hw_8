84b9b8caefad9378f848e1d392c9e6143ebff71c
Ekaterina Goncharenko <eg93006@gmail.com>
2024-07-17 22:30:18 +0300

    banana commit

---
+++ bananaCoder.js
@@ -0,0 +1,17 @@
+// task 1
+console.log('I am a programmer!\nHello\ni\nam\nKate');
+console.log("I am a programmer!\nHello\ni\nam\nKate");
+console.log(`I am a programmer!\nHello\ni\nam\nKate`);
+
+let sentence = 'I am a programmer!\nHello\ni\nam\nKate'
+console.log(sentence);
+
+// task 2 "ba_a_a"
+//option 1
+console.log(String.fromCharCode(98, 97, 110, 97, 110, 97));
+
+//option 2 base64
+console.log(atob('YmFuYW5h'));
+
+//option 3
+console.log('\uD83C\uDF4C');
